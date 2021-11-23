2c816f47aec6c9431b5074b7732542dc31e5bec1
Son Tran Thien Mac <sontmac@Sons-MacBook-Air.local>
2021-11-23 03:23:16 -0700

    Added simple pagination and fixed/added additional styling

--- src/Tickets.js
+++ src/Tickets.js
@@ -1,13 +1,25 @@
 import React, { useState, useEffect } from "react";
 import Ticket from "./Ticket";
-import { Row, Container } from "reactstrap";
+import {
+  Row,
+  Container,
+  Button,
+  Pagination,
+  PaginationItem,
+  PaginationLink,
+} from "reactstrap";
 
 function Tickets() {
   const [tickets, setTickets] = useState([]);
-  const [page, setPage] = useState(2);
+  const [page, setPage] = useState(1);
+  const [perPage, setPerPage] = useState(20);
+  const [numTickets, setNumTickets] = useState([]);
+
+  const numPages = (num, per) => Math.floor((num - 1) / per) + 1;
+
   useEffect(() => {
     fetch(
-      `https://zccsontmac.zendesk.com/api/v2/tickets?page=${page}&per_page=20`,
+      `https://zccsontmac.zendesk.com/api/v2/tickets?page=${page}&per_page=${perPage}`,
       {
         headers: {
           Authorization:
@@ -17,19 +29,57 @@ function Tickets() {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setTickets(data.tickets);
+        setNumTickets(data.count);
       });
-  }, []);
+  }, [page]);
 
   return (
-    <Container className="my-5">
-      <Row>
-        {tickets &&
-          tickets.length > 0 &&
-          tickets.map((ticket) => <Ticket ticket={ticket} />)}
-      </Row>
-    </Container>
+    <div>
+      <Pagination className="mt-5 d-flex justify-content-center">
+        <PaginationItem>
+          <PaginationLink first href="#" onClick={() => setPage(1)} />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#" previous onClick={() => setPage(page - 1)} />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#" next onClick={() => setPage(page + 1)} />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink
+            href="#"
+            last
+            onClick={() => setPage(numPages(numTickets, perPage))}
+          />
+        </PaginationItem>
+      </Pagination>
+      <Container className="my-4">
+        <Row>
+          {tickets &&
+            tickets.length > 0 &&
+            tickets.map((ticket) => <Ticket ticket={ticket} />)}
+        </Row>
+      </Container>
+      <Pagination className="mb-5 d-flex justify-content-center">
+        <PaginationItem>
+          <PaginationLink first href="#" onClick={() => setPage(1)} />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#" previous onClick={() => setPage(page - 1)} />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#" next onClick={() => setPage(page + 1)} />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink
+            href="#"
+            last
+            onClick={() => setPage(numPages(numTickets, perPage))}
+          />
+        </PaginationItem>
+      </Pagination>
+    </div>
   );
 }
 
