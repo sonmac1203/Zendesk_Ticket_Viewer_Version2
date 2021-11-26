4c3695bb5de116132abc676e35d66ffb43c85183
Son Tran Thien Mac <sontmac@Sons-MacBook-Air.local>
2021-11-26 15:38:20 -0700

    Fixed tests for loading spinners

--- src/Loading.js
+++ src/Loading.js
@@ -3,7 +3,7 @@ import { Spinner } from "reactstrap";
 
 const Loading = () => {
   return (
-    <div className="text-center customloading">
+    <div className="text-center customloading" data-testid="loading-spinners">
       <Spinner type="grow" className="mx-2" size="sm" />
       <Spinner type="grow" className="mx-2" size="sm" />
       <Spinner type="grow" className="mx-2" size="sm" />
---
+++ src/tests/Loading.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Loading from "../Loading";
+
+describe("Ticket components", () => {
+  render(<Loading />);
+  const wrapper = screen.getByTestId("loading-spinners");
+
+  test("Should have 10 spinners - first method", () => {
+    expect(wrapper.getElementsByClassName("spinner-grow").length).toBe(10);
+  });
+
+  test("Should have 10 spinners - second method", () => {
+    expect(wrapper.querySelectorAll('[role="status"]')).toHaveLength(10);
+  });
+
+  test("Should contain class 'customloading'", () => {
+    expect(wrapper.classList.contains("customloading")).toBe(true);
+  });
+});
